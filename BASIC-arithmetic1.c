7024cb079db1f771ab69850e31efac5202b21a7f
TibiMarinescu <64827320+TibiMarinescu@users.noreply.github.com>
2021-07-12 11:10:47 +0300

    Create BASIC-arithmetic1.c

---
+++ BASIC-arithmetic1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+int main(){
+    printf("Dear Procrastinator, \n You still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!", 25-23, 60*24*(25-23),60*60*24*(25-23));
+    return 0;
+}
