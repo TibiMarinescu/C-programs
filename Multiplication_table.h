11118ff28cad1281b44265a5d5aab5c070034084
TibiMarinescu <64827320+TibiMarinescu@users.noreply.github.com>
2021-07-22 12:20:05 +0300

    Create Multiplication_table.h

---
+++ Multiplication_table.h
@@ -0,0 +1,18 @@
+//Multiplication table
+#include <stdio.h>
+int main(){
+    int user_number;
+    scanf("%d", &user_number);
+    printf("0x%d = %d\n", user_number, 0*user_number);
+    printf("1x%d = %d\n", user_number, 1*user_number);
+    printf("2x%d = %d\n", user_number, 2*user_number);
+    printf("3x%d = %d\n", user_number, 3*user_number);
+    printf("4x%d = %d\n", user_number, 4*user_number);
+    printf("5x%d = %d\n", user_number, 5*user_number);
+    printf("6x%d = %d\n", user_number, 6*user_number);
+    printf("7x%d = %d\n", user_number, 7*user_number);
+    printf("8x%d = %d\n", user_number, 8*user_number);
+    printf("9x%d = %d\n", user_number, 9*user_number);
+    printf("10x%d = %d\n", user_number, 10*user_number);
+    return 0;
+}
